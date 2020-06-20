b267cb970ca9d583ce5a3041e2a52165ec782dd4
Jonakralisha <66684253+Jonakralisha@users.noreply.github.com>
2020-06-20 13:03:52 +0530

    Add files via upload

---
+++ listtuple.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list=[]
+list.append('1')
+list.append('2')
+list.append('3')
+list
+
+
+# In[2]:
+
+
+Names=('kenneth','jone','jerin')
+Names[1]
+
+
+# In[4]:
+
+
+num=(1,2,3)
+print "num[0:1]:",num[0:1]
+
+
+# In[9]:
+
+
+jonedict={
+    "hey":15,
+    "whatsup":25,
+    "have fun":79,
+    "bad":90,
+}
+del jonedict["have fun"]
+print(jonedict)
+
+
+# In[ ]:
+
+
+
+
